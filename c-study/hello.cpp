2b1878dbbfb28e81d68dd28937df7bc180c6ce0f
yukinissie <bigtree2490@outlook.jp>
2021-08-09 00:07:47 +0900

    苦C 練習問題5 プログラム書き取り

--- c-study/hello.cpp
+++ c-study/hello.cpp
@@ -1,6 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, world!");
+    // 198‰~ ´—Áˆù—¿…
+    int amount_of_juice = 198;
+    // 138‰~ ‹“û
+    int amount_of_milk = 138;
+    // 1000‰~ x•¥‹àŠz
+    int amount_of_paid = 1000;
+    // 0.05 Á”ïÅ
+    double consumption_tax = 0.05;
+    // Å”²‚«‰¿Ši
+    int tax_excluded_price = amount_of_juice + amount_of_milk * 2;
+    // Å‚İ‰¿Ši
+    int tax_included_price = (int)(tax_excluded_price + tax_excluded_price * consumption_tax);
+    // ‚¨’Ş‚è‚Í®”
+    int change = amount_of_paid - tax_included_price;
+    printf("‚¨’Ş‚èF%d\n", change);
     return 0;
 }
